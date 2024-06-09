53bf79d8080ca283249ee944d634044a878396cc
nikkolas <minton.nikkolas@gmail.com>
2024-06-09 06:38:22 -0500

    changed the naming convention of process nodes to be just 'Process' rather than 'ProcessInstance'

--- src/smartCluster.ts
+++ src/smartCluster.ts
@@ -1,14 +1,14 @@
 import { fork, ChildProcess } from "child_process";
 
-class ProcessInstance {
+class Process {
   //For creating doubly linked list nodes for a process queue.
   //The doubly characteristic is for the goal of using a hashmap
   //to quickly reference nodes on their pid
 
-  processReference: ChildProcess | null = null;
+  reference: ChildProcess | null = null;
   pid: number | null;
-  next: ProcessInstance | null = null;
-  previous: ProcessInstance | null = null;
+  next: Process | null = null;
+  previous: Process | null = null;
 
   clearDoublyPointers(): void {
     this.previous = null;
@@ -16,7 +16,7 @@ class ProcessInstance {
   }
 
   clearAll(): void {
-    this.processReference = null;
+    this.reference = null;
     this.pid = null;
     this.previous = null;
     this.next = null;
@@ -24,52 +24,52 @@ class ProcessInstance {
 }
 
 class ProcessQueue {
-  #queueHead: ProcessInstance | null = null;
-  #queueTail: ProcessInstance | null = null;
+  #queueHead: Process | null = null;
+  #queueTail: Process | null = null;
   #processesInQueue: Set<number> = new Set();
 
   //adds to the back of the queue, while also altering the head and tail pointers, as well as
   //the individual process instance previous and next pointers based on necessity.
-  addToQueue(processInstance: ProcessInstance): void {
-    if (!(processInstance.processReference instanceof ChildProcess)) {
+  addToQueue(process: Process): void {
+    if (!(process.reference instanceof ChildProcess)) {
       throw new Error(); //STILL NEED TO ADD CUSTOM ERROR
 
       //to ensure the process is a valid process, and not null
       //FATAL ERROR
     }
 
-    if (typeof processInstance.pid !== "number") {
+    if (typeof process.pid !== "number") {
       throw new Error(); //STILL NEED TO ADD CUSTOM ERROR
 
       //to ensure that the process instance has a valid PID, which is crucial for process tracking
       //FATAL ERROR
     }
 
-    if (this.#processesInQueue.has(processInstance.pid)) {
+    if (this.#processesInQueue.has(process.pid)) {
       throw new Error(); //STILL NEED TO ADD CUSTOM ERROR
 
       //to ensure that processes already in queue aren't added again
       //FATAL ERROR
     }
 
-    if (!(this.#queueHead instanceof ProcessInstance)) {
-      this.#queueHead = this.#queueTail = processInstance;
+    if (!(this.#queueHead instanceof Process)) {
+      this.#queueHead = this.#queueTail = process;
 
       //queue length is 0
     } else {
-      processInstance.previous = this.#queueTail;
-      this.#queueTail!.next = processInstance;
-      this.#queueTail = processInstance;
+      process.previous = this.#queueTail;
+      this.#queueTail!.next = process;
+      this.#queueTail = process;
 
       //queue length >0
     }
 
-    this.#processesInQueue.add(processInstance.pid);
+    this.#processesInQueue.add(process.pid);
   }
 
   //returns the front of the queue, while also altering the head and tail pointers, as well as
   //the individual process instance previous and next pointers based on necessity.
-  shiftFromQueue(): ProcessInstance | null {
+  shiftFromQueue(): Process | null {
     if (!(this.#queueHead instanceof Task)) {
       return null;
 
@@ -96,15 +96,15 @@ class ProcessQueue {
   }
 
   //determines how to alter the queue to remove he supplied process instance from such.
-  removeFromQueue(processInstance: ProcessInstance): void {
-    if (typeof processInstance.pid !== "number") {
+  removeFromQueue(process: Process): void {
+    if (typeof process.pid !== "number") {
       throw new Error(); //STILL NEED TO ADD CUSTOM ERROR
 
       //to ensure that the process instance has a valid PID, which is crucial for process tracking
       //FATAL ERROR
     }
 
-    if (!this.#processesInQueue.has(processInstance.pid)) {
+    if (!this.#processesInQueue.has(process.pid)) {
       throw new Error(); //STILL NEED TO ADD CUSTOM ERROR
 
       //to ensure that the process trying to be removed is actually in the queue
@@ -112,30 +112,30 @@ class ProcessQueue {
     }
 
     if (
-      processInstance === this.#queueHead &&
-      processInstance === this.#queueTail
+      process === this.#queueHead &&
+      process === this.#queueTail
     ) {
       this.#queueHead = this.#queueTail = null;
 
       //if the node is both the head and tail (queue length of 1)
-    } else if (processInstance === this.#queueHead) {
+    } else if (process === this.#queueHead) {
       this.#queueHead = this.#queueHead.next;
       this.#queueHead!.previous = null;
 
       //if the node is the current head
-    } else if (processInstance === this.#queueTail) {
+    } else if (process === this.#queueTail) {
       this.#queueTail = this.#queueTail.previous;
       this.#queueTail!.next = null;
 
       //if the node is the current tail
     } else {
-      processInstance.previous!.next = processInstance.next;
-      processInstance.next!.previous = processInstance.previous;
+      process.previous!.next = process.next;
+      process.next!.previous = process.previous;
 
       //if the node is in the middle of the queue
     }
 
-    this.#processesInQueue.delete(processInstance.pid);
+    this.#processesInQueue.delete(process.pid);
   }
 }
 
@@ -272,8 +272,8 @@ class SmartCluster {
 
   //for managing each valid process object, which contains the actual process reference internally,
   //as well as the doubly pointers used in the process queue.
-  #processInstanceMap = new Map<number, ProcessInstance>();
-  #emptyProcessInstances: ProcessInstance[] = [];
+  #processInstanceMap = new Map<number, Process>();
+  #emptyProcessInstances: Process[] = [];
 
   //for managing empty task objects and number of task objects present,
   //because options for defining preallocated tasks objects, as well as an upper limit to the
